994fd22c191892c4c844104b76c26c1fc5fad9e5
Sreepad Ramesan Kutty <sreepadr@gmail.com>
2015-05-20 15:39:58 -0700

    Added more comments: Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,70 +1,68 @@
-############################################################################### 
+###############################################################################
 ## SCRIPT cachematrix.R
-## 
 ## There are two functions in this script:
-## 1. makeCacheMatrix: This function creates a special "matrix" object that 
-##    can cache its inverse, and returns a list of "get" and "set" functions.
-## 2. cacheSolve: This function computes the inverse of the above "matrix" 
-##    or retrieves it from cache if available, and returns it.
+## 1. makeCacheMatrix: This function creates a special object that can cache
+##    a matrix its inverse, and returns a list of get and set functions.
+## 2. cacheSolve: This function retrieves the inverse of the above mentioned 
+##    matrix from cache if available, or computes it , and returns it.
 ##    Throws error if the input matrix is not invertible.
-##
 ## NOTE: See section SAMPLE RUN at the end of the script for test outputs.
 ###############################################################################
 
-## FUNCTION makeCacheMatrix (x = matrix())
-## Creates a special "matrix" object that can cache its inverse, and  
-## returns a list of "get" and "set" functions for the matrix and its inverse.
-## x - holds the input matrix
-## inv - holds the inverse of the matrix
-## set() - sets the input matrix to x and caches it
-## get() - returns the matrix, held by x
-## setInverse() - sets the inverse of matrix held by x to inv and caches it
-## getInverse () - returns the inverse, held by inv
+## FUNCTION makeCacheMatrix (matrix = matrix())
+## Creates a special object that can cache a matrix and its inverse, and  
+## returns a list of get and set functions for the matrix and its inverse.
+## matrix - holds the input matrix in cache
+## inverse - holds the inverse of the matrix in cache
+## set() - sets the value of matrix and caches it
+## get() - returns the matrix, held by matrix
+## setInverse() - sets the value of inverse and caches it
+## getInverse () - returns the inverse, held by inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-    inv <- NULL
-    set <- function(y) {
-        x <<- y            ## cache the input matrix
-        inv <<- NULL       ## clear the inverse from cache when matrix changes
+makeCacheMatrix <- function(matrix = matrix()) {
+    inverse <- NULL
+    set <- function(input) {
+        matrix <<- input     ## cache the input matrix
+        inverse <<- NULL     ## clear inverse from cache when matrix changes
     }
     get <- function() {
-        x
+        matrix
     }
-    setInverse <- function(inverse) {
-        inv <<- inverse               ## cache the inverse
+    setInverse <- function(inv) {
+        inverse <<- inv                  ## cache the inverse
     }       
     getInverse <- function() {
-        inv
+        inverse
     }
     list(set = set, 
          get = get,
          setInverse = setInverse,
-         getInverse = getInverse) ## return list of all get and set functions
+         getInverse = getInverse)        ## return list of all functions
 }
 
-## FUNCTION cacheSolve(x, ...)
-## Computes and returns the inverse of the matrix returned by makeCacheMatrix.
-## If the inverse has already been computed (and the matrix has not changed),
-## then retrieves the inverse from the cache and returns it.
+## FUNCTION cacheSolve(cacheMatrix)
+## Retrieves and returns the inverse of the matrix cached by makeCacheMatrix,
+## if the inverse has already been computed and the matrix has not changed. If
+## not, computes the inverse newly and returns it.
 ## Throws error while calling solve() if the input matrix is not invertible.
 
-cacheSolve <- function(x, ...) {
-    inv <- x$getInverse()               ## get the inverse from cache...
-    if(!is.null(inv)) {                 ## and if it is not null...
-        message("getting cached data")  ## show a message, and...
-        return(inv)                     ## return it
+cacheSolve <- function(cacheMatrix) {
+    inverse <- cacheMatrix$getInverse()        ## get the inverse from cache...
+    if(!is.null(inverse)) {                    ## and if it is not null...
+        message("getting cached data")         ## show a message, and...
+        return(inverse)                        ## return it
     }
-    data <- x$get()                     ## if not, take the matrix...
-    inv <- solve(data, ...)             ## and compute its inverse...
-    x$setInverse(inv)                   ## set it in cache, and...
-    inv                                 ## return it
+    matrix <- cacheMatrix$get()                ## if not, take the matrix...
+    inverse <- solve(matrix)                   ## and compute its inverse...
+    cacheMatrix$setInverse(inverse)            ## set it in cache, and...
+    inverse                                    ## return it
 }
 
 ###############################################################################
 ## SAMPLE RUN
 ## > source("cachematrix.R")
-## > mcm <- makeCacheMatrix() # create a special matrix using makeCacheMatrix
-## > mcm$set(matrix(c(2,2,3,2),2,2)) # set some data in it
+## > mcm <- makeCacheMatrix() # create the special object using makeCacheMatrix
+## > mcm$set(matrix(c(2,2,3,2),2,2)) # set some matrix in it
 ## > mcm$get() # let us see if it got set correctly
 ##      [,1] [,2]
 ## [1,]    2    3
@@ -73,22 +71,30 @@ cacheSolve <- function(x, ...) {
 ##      [,1] [,2]
 ## [1,]   -1  1.5
 ## [2,]    1 -1.0
-## > mcm$getInverse() # see if the inverse got set in the special matrix
-##                    # and we can retrive it using getInverse
+## > mcm$getInverse() # see if the inverse got set correctly
 ##      [,1] [,2]
 ## [1,]   -1  1.5
 ## [2,]    1 -1.0
-## > mcm$set(mcm$getInverse()) # force a change in the data
-##                             # let us pass the inverse for it
+## > cacheSolve(mcm) # run cacheSolve again to see if we are getting 
+##                   # the inverse from cache
+## getting cached data
+##      [,1] [,2]
+## [1,]   -1  1.5
+## [2,]    1 -1.0
+## > mcm$set(mcm$getInverse()) # force a change in the matrix
+##                             # let us use the inverse for it
+## > mcm$get() # run get to see if the inverse got set to matrix
+##      [,1] [,2]
+## [1,]   -1  1.5
+## [2,]    1 -1.0
 ## > mcm$getInverse() # run getInverse and see if the cache got cleared
 ## NULL
 ## > cacheSolve(mcm) # run cacheSolve again, we should get our original matrix
 ##      [,1] [,2]
 ## [1,]    2    3
 ## [2,]    2    2
-## > mcm$getInverse() # finally, run getInverse again
+## > mcm$getInverse() # finally, let us run getInverse again
 ##      [,1] [,2]
 ## [1,]    2    3
 ## [2,]    2    2
-## >
 ###############################################################################
